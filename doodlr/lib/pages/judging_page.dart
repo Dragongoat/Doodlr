755c2e0f6f0619750814ae7826c8c9e154067fa0
dragongoat <japawek@gmail.com>
2020-05-03 17:14:20 -0700

    Judging page design completed

--- doodlr/lib/pages/judging_page.dart
+++ doodlr/lib/pages/judging_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+enum SelectedMedal {
+  NONE,
+  BRONZE,
+  SILVER,
+  GOLD
+}
 
 class JudgingWidget extends StatefulWidget {
 
@@ -7,6 +15,67 @@ class JudgingWidget extends StatefulWidget {
 }
 
 class _JudgingWidgetState extends State<JudgingWidget> {
+
+  SelectedMedal _selectedMedal = SelectedMedal.NONE;
+  int _curBronze;
+  int _curSilver;
+  int _curGold;
+  var _drawings = [
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.black
+  ];
+
+  void _changeVote(index) {
+    setState(() {
+      switch (_selectedMedal) {
+        case SelectedMedal.BRONZE:
+          _drawings[index] = Colors.deepOrange;
+          if (_curBronze != null) {
+            _drawings[_curBronze] = Colors.black;
+          }
+          if (_curSilver == index) {
+            _curSilver = null;
+          }
+          if (_curGold == index) {
+            _curGold = null;
+          }
+          _curBronze = index;
+          break;
+        case SelectedMedal.SILVER:
+          _drawings[index] = Colors.grey;
+          if (_curSilver != null) {
+            _drawings[_curSilver] = Colors.black;
+          }
+          if (_curBronze == index) {
+            _curBronze = null;
+          }
+          if (_curGold == index) {
+            _curGold = null;
+          }
+          _curSilver = index;
+          break;
+        case SelectedMedal.GOLD:
+          _drawings[index] = Colors.amber;
+          if (_curGold != null) {
+            _drawings[_curGold] = Colors.black;
+          }
+          if (_curBronze == index) {
+            _curBronze = null;
+          }
+          if (_curSilver == index) {
+            _curSilver = null;
+          }
+          _curGold = index;
+          break;
+        case SelectedMedal.NONE:
+          break;
+      }
+    });
+  }
 
   Widget _showPage() {
     return Container(
@@ -17,11 +86,190 @@ class _JudgingWidgetState extends State<JudgingWidget> {
           ),
         ),
         constraints: BoxConstraints.expand(),
-        child: Column(
-          children: [
-            _showInfoBox(),
-          ],
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _showInfoBox(),
+                _showDrawings(),
+              ],
+            ),
+          ),
         )
+    );
+  }
+
+  Widget _showDrawings() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 15.0),
+      child: Column(
+        children: <Widget>[
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(0),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[0], width: (_drawings[0] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[0],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15.0,),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(1),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[1], width: (_drawings[1] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[1],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0,),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(2),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[2], width: (_drawings[2] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[2],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15.0,),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(3),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[3], width: (_drawings[3] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[3],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0,),
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(4),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[4], width: (_drawings[4] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[4],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15.0,),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => _changeVote(5),
+                    child: Container(
+                      child: SizedBox(height: 150.0,),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: _drawings[5], width: (_drawings[5] == Colors.black) ? 2.0 : 4.0),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: _drawings[5],
+                            blurRadius: 10.0,
+                            spreadRadius: 1.0,
+                            offset: Offset(
+                              5.0,
+                              5.0,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -31,6 +279,7 @@ class _JudgingWidgetState extends State<JudgingWidget> {
       child: FractionallySizedBox(
         widthFactor: 0.9,
         child: Container(
+          padding: EdgeInsets.only(left: 50.0, right: 50.0),
           decoration: BoxDecoration(
             border: Border.all(color: Colors.black, width: 2.0),
             color: Colors.green[300],
@@ -59,13 +308,92 @@ class _JudgingWidgetState extends State<JudgingWidget> {
               Text("Tap a medal to assign it to a drawing:"),
               const SizedBox(height: 10),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Image(
-                    image: AssetImage("assets/bronze.png"),
+                  ClipOval(
+                    child: Material(
+                      color: Colors.lightBlueAccent[100],
+                      child: Center(
+                        child: Ink(
+                          decoration: ShapeDecoration(
+                            color: (_selectedMedal == SelectedMedal.BRONZE) ? Colors.green[100] : Colors.green[300],
+                            shape: (_selectedMedal == SelectedMedal.BRONZE) ?
+                            CircleBorder(
+                              side: BorderSide(
+                                  width: 1.0,
+                                  color: Colors.deepOrange,
+                              ),
+                            ) : CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(FontAwesome5Solid.medal),
+                            color: Colors.deepOrange,
+                            onPressed: () {
+                              setState(() {
+                                _selectedMedal = SelectedMedal.BRONZE;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                  Text("Silver"),
-                  Text("Bronze")
+                  ClipOval(
+                    child: Material(
+                      color: Colors.lightBlueAccent[100],
+                      child: Center(
+                        child: Ink(
+                          decoration: ShapeDecoration(
+                            color: (_selectedMedal == SelectedMedal.SILVER) ? Colors.green[100] : Colors.green[300],
+                            shape: (_selectedMedal == SelectedMedal.SILVER) ?
+                            CircleBorder(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Colors.grey,
+                              ),
+                            ) : CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(FontAwesome5Solid.medal),
+                            color: Colors.grey,
+                            onPressed: () {
+                              setState(() {
+                                _selectedMedal = SelectedMedal.SILVER;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ClipOval(
+                    child: Material(
+                      color: Colors.lightBlueAccent[100],
+                      child: Center(
+                        child: Ink(
+                          decoration: ShapeDecoration(
+                            color: (_selectedMedal == SelectedMedal.GOLD) ? Colors.green[100] : Colors.green[300],
+                            shape: (_selectedMedal == SelectedMedal.GOLD) ?
+                            CircleBorder(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Colors.amber,
+                              ),
+                            ) : CircleBorder(),
+                          ),
+                          child: IconButton(
+                            icon: Icon(FontAwesome5Solid.medal),
+                            color: Colors.amber,
+                            onPressed: () {
+                              setState(() {
+                                _selectedMedal = SelectedMedal.GOLD;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
               const SizedBox(height: 10),
